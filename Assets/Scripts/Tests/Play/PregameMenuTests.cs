faf5caff4f84e5dd07b2a63dbfa11a9c8b3c076c
argentumvir <argentumvir@gmail.com>
2021-09-20 18:37:37 -0700

    Added test helpers

--- Assets/Scripts/Tests/Play/PregameMenuTests.cs
+++ Assets/Scripts/Tests/Play/PregameMenuTests.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
+using TMPro;
 
 public class PregameMenuTests : BasePlayTest
 {
@@ -13,5 +14,23 @@ public class PregameMenuTests : BasePlayTest
         yield return StartAtMainMenu();
         ClickButton("PlayButton");
         yield return new WaitForSceneLoaded("PregameScene", .5f);
+    }
+
+    [UnityTest]
+    public IEnumerator UserCanSetNameAndHostGame()
+    {
+        yield return UserCanGetToPregameMenu();
+
+        string playerName = "SomeGuyName";
+        SetTextInput("PlayerNameInput", playerName);
+        ClickButton("HostButton");
+        yield return new WaitForSceneLoaded("MatchScene", .5f);
+
+        string actualPlayerName = GetGameObjectComponent<TextMeshProUGUI>("PlayerName").text;
+
+        Assert.AreEqual(playerName, actualPlayerName);
+
+        string actualOpponentName = GetGameObjectComponent<TextMeshProUGUI>("OpponentName").text;
+        Assert.AreEqual("", actualOpponentName);
     }
 }
--- Assets/Scripts/Tests/Play/TestHelpers/BasePlayTest.cs
+++ Assets/Scripts/Tests/Play/TestHelpers/BasePlayTest.cs
@@ -4,14 +4,49 @@ using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 using NUnit.Framework;
 using UnityEngine.EventSystems;
- using UnityEngine.UI;
+using UnityEngine.UI;
+using TMPro;
+using MLAPI;
+
 public class BasePlayTest
 {
-    private bool hasSceneLoaded = false;
     protected IEnumerator StartAtMainMenu()
     {
         SceneManager.LoadScene("MainMenuScene");
         yield return new WaitForSceneLoaded("MainMenuScene", .5f);
+    }
+
+    protected static void SetTextInput(string inputName, string text, bool verify = true)
+    {
+        GetGameObjectComponent<TMP_InputField>(inputName).text = text;
+    }
+
+
+    protected static GameObject GetGameObject(string name)
+    {
+        GameObject obj = GameObject.Find(name);
+
+        if (obj == null)
+        {
+            throw new ComponentNotFoundException($"GameObject with name '{name}' not found.");
+        }
+
+        return obj;
+    }
+
+    
+    protected static T GetGameObjectComponent<T>(string name)
+    {
+        GameObject obj = GetGameObject(name);
+
+        T objComponent = obj.GetComponent<T>();
+        
+        if (objComponent == null)
+        {
+            throw new ComponentNotFoundException($"Component of type '{typeof(T)}' not found.");
+        }
+
+        return objComponent;
     }
 
     protected static void ClickButton(string buttonName)
